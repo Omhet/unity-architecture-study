2750a9107021371b941d56f30130fbb4c7112358
Omhet <omhetehmo@yandex.ru>
2026-05-22 23:37:36 +0200

    register menu

--- Assets/Scripts/Boot/MenuLifetimeScope.cs
+++ Assets/Scripts/Boot/MenuLifetimeScope.cs
@@ -1,9 +1,17 @@
+using Flow.Menu;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
+using View.Menu;
 
 public class MenuLifetimeScope : LifetimeScope
 {
+    [SerializeField] private MainMenuView _mainMenuView;
+
     protected override void Configure(IContainerBuilder builder)
     {
+        builder.RegisterComponent(_mainMenuView);
+
+        builder.RegisterEntryPoint<MenuController>();
     }
 }
