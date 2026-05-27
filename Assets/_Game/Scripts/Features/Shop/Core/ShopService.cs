f7dcdf2d40906f246e3a62e93680ba57f58736a2
Omhet <omhetehmo@yandex.ru>
2026-05-27 16:23:34 +0200

    delete not used inventory model

--- Assets/_Game/Scripts/Features/Shop/Core/ShopService.cs
+++ Assets/_Game/Scripts/Features/Shop/Core/ShopService.cs
@@ -1,23 +1,13 @@
-using App.Economy.Core;
-using App.Inventory.Core;
-using System.Linq;
-
 namespace App.Shop.Core
 {
     public class ShopService
     {
         private readonly ShopModel _shopModel;
-        private readonly EconomyService _economyService;
-        private readonly InventoryService _inventoryService;
 
         public ShopService(
-            ShopModel shopModel,
-            EconomyService economyService,
-            InventoryService inventoryService)
+            ShopModel shopModel)
         {
             _shopModel = shopModel;
-            _economyService = economyService;
-            _inventoryService = inventoryService;
         }
 
         public void LoadCatalog(ShopConfig config)
@@ -30,23 +20,6 @@ namespace App.Shop.Core
                     _shopModel.Items.Add(item);
                 }
             }
-        }
-
-        public bool TryBuyItem(string itemId)
-        {
-            // 1. Find Item
-            ShopItem itemToBuy = _shopModel.Items.FirstOrDefault(x => x.ItemId == itemId);
-            if (itemToBuy == null) return false;
-
-            // 2. Validate and Spend Currency
-            if (_economyService.TrySpend(itemToBuy.Cost))
-            {
-                // 3. Give Item
-                _inventoryService.AddItem(itemToBuy.ItemId, 1);
-                return true;
-            }
-
-            return false;
         }
     }
 }
