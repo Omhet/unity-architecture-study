f007c3c82bb05560da0fa43833d2e8f05a3951e4
Omhet <omhetehmo@yandex.ru>
2026-06-04 13:51:17 +0200

    add NaughtyAttributes

--- Assets/_Game/Scripts/Systems/Debug/DebugHelper.cs
+++ Assets/_Game/Scripts/Systems/Debug/DebugHelper.cs
@@ -3,13 +3,14 @@ namespace App.Systems.Debug
     using App.Progression.Core;
     using UnityEngine;
     using VContainer;
+    using NaughtyAttributes;
 
     public class DebugHelper : MonoBehaviour
     {
         [Inject]
         public ProgressionService _progressionService;
 
-        [ContextMenu("Add 50 XP")]
+        [Button("Add 50 XP")]
         private void AddXP()
         {
             _progressionService.AddXp(50);
